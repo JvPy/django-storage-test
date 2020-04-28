96296b69ba3f6277a2ff7c444021a9abebb20a3d
JvPy <jvmartinellis@gmail.com>
2020-04-27 21:00:17 -0300

    initial commit

---
+++ installsteps.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+sudo apt-get install virtualenv
+
+virtualenv -p python3 env
+
+virtualenv -p python3 env
+
+## Enter virtualenv
+
+source venv/bin/activate
+
+pip3 install Django
+
+django-admin startproject storage
+
+pip3 install django-storages
+
+pip3 install boto
